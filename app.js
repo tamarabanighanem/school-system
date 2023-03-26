e0d3f137f26c1f1d668bc0bc7fca71267f33ed06
tamara <tbanighanem@gmail.com>
2023-03-27 02:34:51 +0300

    newadd

--- app.js
+++ app.js
@@ -1,40 +1,70 @@
-let form=document.getElementById("myform");
-let table=document.getElementById("mytable");
+let arrData = [];
+if (localStorage.arrData != null) {
+    arrData = JSON.parse(localStorage.arrData);
+    render();
+}
+
+let form = document.getElementById("myform");
+let divcont = document.getElementById("stdinfo");
 form.addEventListener("submit", handeleSubmit);
 
-function handeleSubmit(event){
+function handeleSubmit(event) {
     event.preventDefault();
-    let firstrname= event.target.fname.value;
-    let gradee=event.target.grade.value;
-    let phonee=event.target.phone.value;
-    let gender=event.target.optradio.value;
-    let bairthday=event.target.bair.value;
-console.log(firstrname,gradee,phonee,gender,bairthday);
+    let firstrname = event.target.fname.value;
+    let gradee = event.target.grade.value;
+    let phonee = event.target.phone.value;
+    let gender = event.target.optradio.value;
+    let bairthday = event.target.bair.value;
+    let imgg = event.target.image.value;
+    let sel = event.target.maj.value;
+    console.log(firstrname, gradee, phonee, gender, bairthday, imgg, sel);
 
-render(firstrname,gradee,phonee,gender,bairthday);}
+    let someone = new Student(firstrname, gradee, phonee, gender, bairthday, imgg, sel);
+    arrData.push(someone);
+    localStorage.setItem('arrData', JSON.stringify(arrData));
+    render();
 
-function render(firstrname,gradee,phonee,gender,bairthday){
-    let row=document.createElement('tr');
-    let td1=document.createElement('td');
-    let td2=document.createElement('td');
-    let td3=document.createElement('td');
-    let td4=document.createElement('td');
-    let td5=document.createElement('td');
 
-    td1.textContent=firstrname;
-    td2.textContent=gradee;
-    td3.textContent=phonee;
-    td4.textContent=gender;
-    td5.textContent=bairthday;
+}
 
-    
-row.appendChild(td1);
-row.appendChild(td2);
-row.appendChild(td3);
-row.appendChild(td4);
-row.appendChild(td5);
+function Student(firstrname, gradee, phonee, gender, bairthday, imgg, sel) {
+    this.firstrname = firstrname;
+    this.gradee = gradee;
+    this.phonee = phonee;
+    this.gender = gender;
+    this.bairthday = bairthday;
+    this.imgg = imgg;
+    this.sel = sel;
 
-table.appendChild(row);
-form.reset ();
 
+
+}
+function render() {
+    for (let i = 0; i < arrData.length; i++) {
+        // let stdinfoo = document.getElementById('stdinfo');
+        let std = document.createElement('div');
+        let img1 = document.createElement('img');
+        img1.src = `${arrData[i].img}`;
+        let ul = document.createElement('ul');
+        let li1 = document.createElement('li');
+        li1.textContent = `Name: ${arrData[i].firstrname}`;
+        let li2 = document.createElement('li');
+        li2.textContent = `Phone: ${arrData[i].phonee}`;
+        let li3 = document.createElement('li');
+        li3.textContent = `Gender: ${arrData[i].gender}`;
+        let li4 = document.createElement('li');
+        li4.textContent = `Bairthday ${arrData[i].bairthday}`;
+        let li5 = document.createElement('li');
+        li5.textContent = `Major ${arrData[i].sel}`;
+        ul.appendChild(li1);
+        ul.appendChild(li2);
+        ul.appendChild(li3);
+        ul.appendChild(li4);
+        ul.appendChild(li5);
+        std.appendChild(img1);
+        std.appendChild(ul);
+        divcont.appendChild(std);
+        form.reset();
+
+    }
 }
